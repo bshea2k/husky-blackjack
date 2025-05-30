37a76a24653c931a93bb0b97a057ccc1e7945442
Bryant Shea <bshea2k@gmail.com>
2025-05-29 20:52:13 -0700

    Implement deck functionality

---
+++ scripts/game-logic.js
@@ -0,0 +1,32 @@
+class Deck {
+    constructor(deckId) {
+        this.deckId = deckId;
+    }
+
+    async init() {
+        const res = await fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
+        const data = await res.json();
+
+        return new Deck(data.deck_id);
+    }
+
+    // shuffles the deck
+    async shuffle() {
+        const res = await fetch(`https://deckofcardsapi.com/api/deck/${this.deckId}/shuffle/`);
+    }
+
+    // draws a card, returns the card drawn
+    async drawCard() {
+        const res = await fetch(`https://deckofcardsapi.com/api/deck/${this.deckId}/draw/?count=1`);
+        const data = await res.json();
+
+        return data.cards[0];
+    }
+}
+async function main() {
+    const deck = await new Deck().init();
+    const card = await deck.drawCard();
+    console.log(card.code);
+}
+
+main();
