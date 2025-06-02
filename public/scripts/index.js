d797f0c2adccbfa06fab798d7537ee6286067e2f
Bryant Shea <bshea2k@gmail.com>
2025-06-02 11:03:43 -0700

    Add a join with code multiplayer screen

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,6 +1,9 @@
 // TO DO
 // - Accessibility: add a tooltip when users hover over a disabled button saying to login to access this feature
 
+const multiplayerButton = document.querySelector("#multiplayer-btn");
+const privateButton = document.querySelector("#private-btn");
+
 document.addEventListener("DOMContentLoaded", domLoaded);
 
 function domLoaded() {
@@ -9,14 +12,27 @@ function domLoaded() {
             enableMultiplayer();
         } 
     });
+
+    multiplayerButton.addEventListener("click", openMultiplayerPopup);
+
+    const multiplayerPopupExit = document.querySelector(".multiplayer-join__exit");
+    multiplayerPopupExit.addEventListener("click", closeMultiplayerPopup);
+
 }
 
 function enableMultiplayer() {
-    const multiplayerButton = document.querySelector("#multiplayer-btn");
-    const privateButton = document.querySelector("#private-btn");
-
     multiplayerButton.disabled = false;
     multiplayerButton.classList.remove("button--disabled");
     privateButton.disabled = false;
     privateButton.classList.remove("button--disabled");
+}
+
+function openMultiplayerPopup() {
+    const multiplayerPopup = document.querySelector(".multiplayer-join");
+    multiplayerPopup.classList.remove("multiplayer-join--hidden");
+}
+
+function closeMultiplayerPopup() {
+    const multiplayerPopup = document.querySelector(".multiplayer-join");
+    multiplayerPopup.classList.add("multiplayer-join--hidden");
 }
