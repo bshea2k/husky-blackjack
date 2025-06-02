885cef53c81f940103eccefbd11803d45db6cf84
Bryant Shea <bshea2k@gmail.com>
2025-06-01 23:47:43 -0700

    Make profile icon a login

---
+++ public/scripts/authentication.js
@@ -0,0 +1,23 @@
+window.addEventListener("DOMContentLoaded", domLoaded);
+
+async function domLoaded() {
+    const profileIcon = document.querySelector(".navigation__profile-icon");
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            profileIcon.setAttribute("src", user.photoURL);
+            console.log("done");
+        } else {
+            console.log("not logged in");
+        } 
+    });
+}
+
+function googleLogin() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(provider)
+        .then(result => {
+            const user = result.user;
+        })
+        .catch(console.log)
+}
